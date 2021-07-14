d69913f6a55984a47b8728ba4ed0281cc4afb5c9
dhairy-online <dhairysrivastava5@gmail.com>
2021-07-14 10:33:29 +0530

    added landing-page

---
+++ js/main.js
@@ -0,0 +1,62 @@
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var button = document.createElement("BUTTON");
+  var txt = document.createTextNode("\u00D7");
+  button.className = "btn btn-danger flex-row-reverse p-2";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(button);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("btn btn-danger");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+ 
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('active');
+    Notification.requestPermission().then(function(result) {
+      console.log(result);
+    });
+    new Notification('Todo app', { body: "To-do has been confirmed" });
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  li.classList.toggle("list-group-item");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("BUTTON");
+  var txt = document.createTextNode("\u00D7");
+  span.appendChild(txt);
+  span.className = "btn btn-danger flex-row-reverse p-2";
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+} 
+
+
+ 
